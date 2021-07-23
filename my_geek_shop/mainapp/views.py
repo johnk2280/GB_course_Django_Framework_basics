b8ab865a01b36e45d0eb757a91aa262999b4d311
johnk2280 <johnk2280@gmail.com>
2021-07-23 21:03:48 +0500

    [add] added context.json

--- my_geek_shop/mainapp/views.py
+++ my_geek_shop/mainapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import json
+
+
+def read_context_file():
+    with open('my_geek_shop/static/my_geek_shop/context.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 
 def render_products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': read_context_file()['products']['title'],
+        'text': read_context_file()['products']['text'],
+        'menu_links': read_context_file()['products']['menu_links'],
+    }
+    return render(request, 'mainapp/products.html', context)
--- my_geek_shop/my_geek_shop/views.py
+++ my_geek_shop/my_geek_shop/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import json
+
+
+def read_context_file():
+    with open('static/my_geek_shop/context.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 
 def render_index(request):
@@ -11,3 +17,10 @@ def render_contacts(request):
 
 def render_prod_description(request):
     return render(request, 'my_geek_shop/prod_description.html')
+
+
+# def render_header_menu(request):
+#     context = {
+#         'header_menu_links': read_context_file()['header_menu_links']
+#     }
+#     return render(request, 'my_geek_shop/includes/inc_header_menu.html', context)
