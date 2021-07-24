ae0b7d3f79f3a29e2c66946368d82237e2cd93d6
johnk2280 <johnk2280@gmail.com>
2021-07-24 16:22:06 +0500

    [add] added models

--- my_geek_shop/mainapp/models.py
+++ my_geek_shop/mainapp/models.py
@@ -2,4 +2,40 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    name = models.CharField(
+        verbose_name='имя',
+        max_length=64,
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE,
+        verbose_name='категория'
+    )
+    name = models.CharField(
+        verbose_name='название продукта',
+        max_length=128,
+    )
+    short_description = models.CharField(
+        verbose_name='краткое описание',
+        max_length=256,
+        blank=True,
+    )
+    img = models.ImageField(
+        upload_to='products_images',
+        blank=True,
+    )
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True,
+    )
+    price = models.DecimalField
