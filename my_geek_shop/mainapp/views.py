3ec3636f20d04509848cc01254643ffe0b5fc7d4
johnk2280 <johnk2280@gmail.com>
2021-07-25 23:56:52 +0500

    [upd] updated file upload function

--- my_geek_shop/mainapp/views.py
+++ my_geek_shop/mainapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+import os
+import csv
+
 from mainapp.models import ProductCategory, Product, ProductsFile
 
 import json
+
+from my_geek_shop.settings import BASE_DIR
 
 
 def get_categories():
@@ -18,19 +23,49 @@ def check_file_upload():
     pass
 
 
-def read_products_file():
-    pass
+def get_products_from_file(file):
+    added_products = []
+    file_path = os.path.join(BASE_DIR, 'media', file)
+    with open(file_path, 'r', encoding='utf-16') as f_obj:
+        file_reader = csv.DictReader(f_obj)
+        for row in file_reader:
+            added_products.append(row)
+
+    # TODO: здесь реализовать через yield
+
+    return added_products
 
 
-def upload_products_file():
+def get_uploaded_products_files():
     uploaded_files = [
         file for file in sorted(ProductsFile.objects.values(), reverse=True) if file['is_uploaded'] == False
     ]
+    # TODO: здесь реализовать выборку через query selectors
     return uploaded_files
+
+
+def add_products():
+    uploaded_files = get_uploaded_products_files()
+    for file in uploaded_files:
+        products = get_products_from_file(file['file'])
+        for product in products:
+            added_product = Product(
+                name=product['name'],
+                short_description=product['short_description'],
+                img=product['img'],
+                description=product['description'],
+                price=product['price'],
+                quantity=product['quantity'],
+                category_id=product['category_id']
+            )
+            added_product.save()
+
+        # TODO: здесь реализовать установки флага 'is_uploaded' в True
 
 
 def get_page_data(page_name):
     data = read_context_file()
+    # TODO: добавить данные в словарь и реализовать динамическое наполнение товарами
     return {
         'title': data[page_name]['title'],
         'text': data[page_name]['text'],
@@ -40,10 +75,11 @@ def get_page_data(page_name):
 
 
 def render_products(request):
-    print(upload_products_file())
+    add_products()
     context = get_page_data('products')
     return render(request, 'mainapp/products.html', context)
 
 
 def get_category(request, pk=None):
+    # TODO: здесь реализовать обработку вызова категорий
     print(pk)
