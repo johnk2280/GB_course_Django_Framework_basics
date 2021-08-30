668097805df19f984a04e26ee58d1ec16882e188
johnk2280 <johnk2280@gmail.com>
2021-08-30 20:47:57 +0500

    [add] added style

--- my_geek_shop/my_geek_shop/settings.py
+++ my_geek_shop/my_geek_shop/settings.py
@@ -126,6 +126,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'my_geek_shop', 'static'),
 )
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
