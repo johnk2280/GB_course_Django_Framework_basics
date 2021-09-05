e75f81074a83d0eba3d6d91e2b42080e6f2bf646
johnk2280 <johnk2280@gmail.com>
2021-09-05 17:02:21 +0500

    [add] added solution for homework to lesson 4 (part 2) task 1

--- my_geek_shop/basketapp/models.py
+++ my_geek_shop/basketapp/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from django.conf import settings
 
 from mainapp.models import Product
+from ordersapp.models import OrderItem
+
+
+class BasketQuerySet(models.QuerySet):
+    def delete(self, *args, **kwargs):
+        for object in self:
+            object.product.quantity += object.quantity
+            object.product.save()
+
+        super(BasketQuerySet, self).delete(*args, **kwargs)
 
 
 class Basket(models.Model):
+    objects = BasketQuerySet.as_manager()
+
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -23,6 +35,10 @@ class Basket(models.Model):
         auto_now_add=True,
     )
 
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.filter(pk=pk).first()
+
     @property
     def get_product_total_cost(self):
         return self.product.price * self.quantity
@@ -34,3 +50,13 @@ class Basket(models.Model):
     @property
     def get_products_quantity(self):
         return sum(product.quantity for product in Basket.objects.filter(user=self.user))
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
+
--- my_geek_shop/ordersapp/views.py
+++ my_geek_shop/ordersapp/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.db import transaction
+from django.db.models.signals import pre_save, pre_delete
 from django.forms import inlineformset_factory
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.dispatch import receiver
 
 from basketapp.models import Basket
 from ordersapp.models import Order, OrderItem
@@ -90,28 +92,28 @@ class OrderItemsUpdateView(LoginRequiredMixin, UpdateView):
             form=OrderItemForm,
             extra=1,
         )
-        
+
         if self.request.POST:
-            formset = OrderFormSet(self.request.POST, isinstance=self.object)
+            formset = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            formset = OrderFormSet(isinstance=self.object)
+            formset = OrderFormSet(instance=self.object)
             data['order_items'] = formset
             return data
-        
+
     def form_valid(self, form):
         context = self.get_context_data()
         order_items = context['order_items']
-        
+
         with transaction.atomic():
             form.instance.user = self.request.user
             self.object = form.save()
             if order_items.is_valid():
                 order_items.instance = self.object
                 order_items.save()
-                
+
         if self.object.get_total_cost() == 0:
             self.object.delete()
-            
+
         return super(OrderItemsUpdateView, self).form_valid(form)
 
 
@@ -123,7 +125,7 @@ class OrderDeleteView(LoginRequiredMixin, DeleteView):
 
 class OrderReadView(LoginRequiredMixin, DetailView):
     model = Order
-    template_name = 'ordersapp/order_detail'
+    template_name = 'ordersapp/order_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super(OrderReadView, self).get_context_data(**kwargs)
@@ -135,4 +137,26 @@ def order_forming_complete(request, pk):
     order = get_object_or_404(Order, pk=pk)
     order.status = Order.SENT_TO_PROCEED
     order.save()
-    return HttpResponseRedirect(reverse('orders:order_list'))
+    return HttpResponseRedirect(reverse('order:orders_list'))
+
+
+# TODO: Исправить - при добавлении/удалении товара в корзине, в базе удаляются сразу 2 шт.
+# TODO: Исправить - при удалении товара из корзины (кнопка remove) товар в базу не возвращается.
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_after_save(sender, update_fields, instance, **kwargs):
+    if update_fields == 'quantity' or 'product':
+        if instance.pk:
+            instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        else:
+            instance.product.quantity -= instance.quantity
+
+        instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_after_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
