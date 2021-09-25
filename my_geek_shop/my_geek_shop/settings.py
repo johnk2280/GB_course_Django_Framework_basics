f3f530e1775d790d05c187cbc12fc4db5f9f322c
johnk2280 <johnk2280@gmail.com>
2021-09-25 17:37:05 +0500

    [upd] updated settings

--- my_geek_shop/my_geek_shop/settings.py
+++ my_geek_shop/my_geek_shop/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost:8000']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -74,7 +74,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.user_details',
 )
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -117,9 +116,14 @@ WSGI_APPLICATION = 'my_geek_shop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_geek_shop',
+        'USER': 'postgres',
     }
 }
 
